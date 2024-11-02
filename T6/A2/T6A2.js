2be98a46b2ebf6a1173a401f7d39aab0dcbc2279
Alf-CS <116028213+Alf-CS@users.noreply.github.com>
2024-11-02 18:06:50 +0100

    Validacion GENERO acabada
    Se añade el id "genero-error" en la label GENERO en el HTML.
    Validaciones para NOMBRE, DNI, TELEFONO y GENERO terminadas

--- T6/A2/T6A2.js
+++ T6/A2/T6A2.js
@@ -14,52 +14,64 @@ document.addEventListener("DOMContentLoaded", () => {
         const nombreUsuario = form.querySelector("input[placeholder='nombre-usuario']");
         const email = form.querySelector("input[placeholder='example@mail.com']");
         
+
+
         let formIsValid = true;
 
         const errors = form.querySelectorAll(".error-text");
         errors.forEach(error => error.textContent = "");
-
         
         // Validación del campo Nombre y apellidos
         const nombreRegex = /^(?=.{4,})([A-Za-zÁÉÍÓÚáéíóúüÜñÑ]+(?: [A-Za-zÁÉÍÓÚáéíóúüÜñÑ]+){1,4})$/;  // faltan vocales abiertas  --> opción alfabeto europeo
         if (!nombreRegex.test(nombreCompleto.value.trim())) {
             formIsValid = false;
-            mostrarError(nombreCompleto, "Por favor, introduce un nombre válido con entre 2 y 5 palabras y sin caracteres especiales.");
+            mostrarError(nombreCompleto, "Por favor, introduce un nombre válido, que tenga entre 2 y 5 palabras y sin caracteres especiales.");
         }
         
-
         // Validación del campo DNI
-        
         const dniRegex = /^\d{8}-[A-Z]$/;  
         if (!dniRegex.test(dni.value.trim())) {
             formIsValid = false;
             mostrarError(dni, "Por favor, introduce un DNI VALIDO: [8 cifras]-[letra].");
         }
         
-
+        // Validación del campo telefono
         const telefonoRegeX = /^\d{3}-\d{2}-\d{2}-\d{2}$/;
         if (!telefonoRegeX.test(telefono.value.trim())) {
-            // formIsValid = false;
+            // formIsValid = false;  -- ya que no es obligatorio.
             mostrarError(telefono, "El telefono introducido no es válido. El formato es 000-00-00-00");
         }
+        
+        // Validación de campo Género
+        const generoMasculino = form.querySelector("input[type='radio']:first-of-type");
+        const generoFemenino = form.querySelector("input[type='radio']:last-of-type");    
+        const generoErrorText = document.getElementById("genero-error");
+        // const generoSeleccionado = form.querySelector("input[name='genero']:checked"); // ASÍ NO ME HA FUNCIONADO     
+        if (!generoMasculino.checked && !generoFemenino.checked) {  //if (!generoSeleccionado) {  esto tampoco funciona
+            generoErrorText.textContent = "Selecciona un género";
+            formIsValid = false;
+        } else {
+                generoErrorText.textContent = ""; // Limpia el mensaje si está seleccionado
+               };
 
-        if (formIsValid) {
+       if (formIsValid) {
             const newRow = document.createElement("tr");
             newRow.innerHTML = `
                 <td>${nombreCompleto.value}</td>
                 <td>${dni.value}</td>
                 <td>${nombreUsuario.value}</td>
                 <td>${email.value}</td>
-            `;
+                `;
             usersList.appendChild(newRow);
             form.reset(); 
         }
     }
 
+
     function mostrarError(input, mensaje) {
         const errorText = input.nextElementSibling; //seleccionamos el span siguiente
         errorText.textContent = mensaje;
-        // errorText.style.color = "red";  El CSS hace básicamente esto, aparte del tamaño de letra.
+        // errorText.style.color = "red";  El CSS  YA hace básicamente esto, aparte del tamaño de letra.
     }
 
     form.querySelector("input[value='Reiniciar']").addEventListener("click", (event) => {
